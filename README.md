981feac2bb2ad48c4a2d20a0dc829bf5c28546f2
JacquesMarais <marais.jacques12@gmail.com>
2018-07-06 10:19:23 +0200

    Revert "Update to new version"
    This reverts commit 15e24d021718d4c5000dc8a56094aa53f70bb95a.

--- README.md
+++ README.md
@@ -2,6 +2,7 @@
 
 [Discord](https://discord.gg/gqgnAcJ)
 
+<<<<<<< HEAD
 Parallex is a fairly new JavaScript framework. Currently it only has a small bit of functionality. Feel free to create issues/pull requests if you want to contribute or contact me on Discord.
 
 # Get Started
@@ -47,4 +48,65 @@ The array is also reactive, meaning that when you update it, the HTML will also 
 As you can see the `fruits` array has been changed and so has the HTML content inside the container, adding the an orange to the list.
 =======
 >>>>>>> parent of d8cb0ee... Update README.md
+=======
+Parallex is a fairly new JavaScript framework. Currently it only has a small bit of functionality. Feel free to create issues/pull requests if you want to contribute.
+
+# Usage
+
+## Getting Started
+
+Download the **parallex.js** file and import it with a script tag into your HTML, before other scripts that use Parallex:
+
+    <script src="parallex.js"></script>
+    
+Next, initialise the Parallex object, using the following syntax:
+
+    new Parallex ( <Object> options );
+    
+Inside the `options`, set your container element (defaults to `document.body`) and your data variables (explained further on).
+
+Here's a simple example of the initialisation:
+
+    new Parallex ({
+      elm: '#container'
+    })
+
+## Reactive Template Engine
+
+Parallex has a simple template engine that works similar to most other JavaScript templating engines. You simply add a data object to your Parallex initialisation function and then specify the data variables inside your HTML to print out the corresponding values in their place. For example:
+
+    <div id="container">
+      [[name]]
+    </div>
+    
+    <script>
+      var parallex = new Parallex({
+        elm: '#container',
+        data: {
+          name: 'Steve'
+        }
+      })
+    </script>
+    
+The content of the `div` will then be `Steve`. The data object is also reactive, which means that when you update a value in the object, the HTML content will also update, for example:
+
+    parallex.data.name = 'John'
+    
+The content of the `div` will now be `John`.
+
+## Events
+
+To add events to your elements using Parallex, simply use the `Parallex.event` method and add a `p-on` attribute to the elements you want to add the event to. This attribute's syntax is `event:handler`. Here is an example:
+
+The HTML:
+    
+    <button p-on='click:changeName'>Change Name To Mary</button>
+
+The JavaScript:
+    
+    parallex.event('changeName', function(){
+        parallex.data.name = 'Mary'
+    })
+    
+>>>>>>> parent of 15e24d0... Update to new version
     
--- parallex.js
+++ parallex.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 var Parallex = function (options){
     var initialise = function (){
         var final = {
@@ -61,14 +62,115 @@ var Parallex = function (options){
                     this.handleType = 'list';
                     this.handle = [object];
                 }
+=======
+function arrayObserve(array, callback, thisArg){
+    if (Array.isArray(array)){
+         var result = [];
+         result.push.apply(result, array);
+     } else {
+         var result = Object.assign({}, array);
+     }
 
+    function getterAndSetter() {
+        if (Array.isArray(array))
+            var currentArray = result;
+        else
+            var currentArray = Object.keys(result);
+        currentArray.forEach( function(prop){
+            if (Array.isArray(result))
+                var currentProp = result.lastIndexOf(prop);
+            else
+                var currentProp = prop;
+
+            /*if (typeof result[currentProp].tagName !== "undefined" && result[currentProp].tagName == "INPUT") {
+                result[currentProp] = ' ';
+                array[currentProp].addEventListener('keyup', function(){
+                    callback.call(thisArg, result);
+                    result[currentProp] = this.value;
+                });
+            } else {*/
+            Object.defineProperty(array, currentProp, {
+                configurable: true,
+                enumerable: true,
+                set: function(val) {
+                    result[currentProp] = val;
+                    callback.call(thisArg, result, currentProp);
+                },
+                get: function() {
+                    return result[currentProp];
+                }
+            });
+            //}
+            if (typeof result[currentProp] == 'object') {
+                arrayObserve(result[currentProp], callback, result);
+            }
+        });
+    }
+    getterAndSetter();
+    if (Array.isArray(array)){
+        ['pop','push','reverse','shift','unshift','splice','sort','map','filter'].forEach( function(method){
+            Object.defineProperty(array, method, {
+                configurable: false,
+                enumerable: false,
+                writable: true,
+                value: function () {
+                    var noReturnMethods = ['push', 'pop', 'reverse', 'shift', 'unshift'];
+                    if (noReturnMethods.indexOf(method) > -1) {
+                        Array.prototype[method].apply(result, arguments);
+                    } else {
+                        var results = Array.prototype[method].apply(result, arguments);
+                        result = results;
+                    }
+                    callback.call(thisArg, result, null);
+                    getterAndSetter();
+                    return result;
+                }
+            });
+        });
+    }
+    return result;
+}
+
+// Define the Parallex object
+var Parallex = function(options) {
+    var main = {
+        // Define the main container from the options, defaults to the document body
+        container: document.querySelector(options.elm) || document.body,
+        // Define lastElement as the main container
+        lastElement: this.container,
+
+        lists: [],
+
+        data: options.data || {},
+        // List method:
+        list: function(name, options) {
+            if (typeof options !== 'object' && typeof name === 'object')
+                options = name;
+
+            if (typeof options.elm !== 'string')
+                // If the element set in the options is not a string, set element to lastElement
+                var element = this.lastElement;
+            else
+                // Otherwise set element to the selector set in options
+                var element = document.querySelector(options.elm);
+
+            if (typeof name !== 'string' && typeof options.name !== 'string')
                 return this;
-            },
+            else if (typeof name !== 'string')
+                name = options.name;
 
-            /**
-                Output Methods
-                                **/
+            this.lastElement = element;
 
+            var parentNode = element.parentNode;
+>>>>>>> parent of 15e24d0... Update to new version
+
+            if (!Array.isArray(options.content))
+                return this;
+
+            var content = options.content,
+                contentDup = options.content;
+
+<<<<<<< HEAD
             // Set the element in which to change data
             in: function (element, run) {
                 elements = [];
@@ -82,39 +184,58 @@ var Parallex = function (options){
                 });
 
                 this.inElement = elements;
+=======
+            var currentContent = '';
 
-                if (run)
-                    this[this.handleType].apply(this, this.handle);
+            while (parentNode.firstChild)
+                parentNode.removeChild(parentNode.firstChild);
+>>>>>>> parent of 15e24d0... Update to new version
 
-                return this;
-            },
+            // Loop through the content
+            var index = 0;
+            while (index < contentDup.length){
 
-            // After a certain time do everything
-            after: function(time, run) {
-                this.waitTime = parseInt(time, 10);
+                currentContent = contentDup[index];
 
-                if (run)
-                    this[this.handleType].apply(this, this.handle);
+                if (typeof currentContent === 'string') {
+                    var clone = element.cloneNode();
 
-                return this;
-            },
+                    clone.innerHTML = currentContent;
 
-            // Run everything
-            run: function() {
-                var that = this;
+                    parentNode.appendChild(clone);
+                }
 
+                index++;
+
+<<<<<<< HEAD
                 function noWait(){
                     that[that.handleType].apply(that, that.handle);
                     that.arrayObserve(that.handle, function(changedHandle){
                         that[that.handleType].apply(that, [changedHandle]);
                     }, that);
                 }
+=======
+            }
+>>>>>>> parent of 15e24d0... Update to new version
 
-                if (this.waitTime > 0) {
-                    setTimeout(noWait, this.waitTime);
-                } else {
-                    noWait();
+            /*this.lists[name] = new Proxy(content, {
+                apply: function(target, thisArg, argumentsList) {
+                    options.content[target].apply(thisArg, argumentsList);
+                    this.list(name, options);
+                    return thisArg[target].apply(thisArg, argumentList);
+                },
+                deleteProperty: function(target, property) {
+                    delete options.content[property];
+                    this.list(name, options);
+                    return true;
+                },
+                set: function(target, property, value, receiver) {
+                    target[property] = value;
+                    options.content[property] = value;
+                    this.list(name, options);
+                    return true;
                 }
+<<<<<<< HEAD
 
                 return this;
             },
@@ -163,11 +284,20 @@ var Parallex = function (options){
 
                 return element;
             },
+=======
+            });*/
+>>>>>>> parent of 15e24d0... Update to new version
 
-            // Run a function for a list of elements or one element
-            doForElementList: function (elements, callback) {
+            if (!this.lists[name]){
+                this.lists[name] = content;
                 var that = this;
+                arrayObserve(this.lists[name], function(result){
+                    options.content = result;
+                    that.list(name, options);
+                }, null);
+            }
 
+<<<<<<< HEAD
                 if ( typeof elements === 'object' &&
                     (/^\[object\s(HTMLCollection|NodeList)\]$/.test( Object.prototype.toString.call(elements) ) ||
                     Array.isArray(elements) ) ) {
@@ -232,37 +362,86 @@ var Parallex = function (options){
                             that.arrayObserve(result[currentProp], callback, result);
                         }
                     });
+=======
+            return this;
+        },
+        parseTemplate: function(){
+            var that = this,
+                attributesToReplace = [];
+            function replaceData(dataVals){
+                document.querySelectorAll('p-data').forEach(function(currentElement){
+                    currentElement.innerHTML = dataVals[currentElement.getAttribute('data-name')];
+                });
+            }
+            replaceData(that.data);
+            var elementIndex = 0;
+            document.querySelectorAll('[p-attributes]').forEach(function(currentElement){
+                var attributes = currentElement.attributes,
+                    index = 0;
+                currentElement.setAttribute('p-attributes', 'p-index-' + elementIndex);
+                while(index < attributes.length) {
+                    var attribute = attributes[index];
+                    if (/^\[\[.*\]\]$/.test(attribute.value)) {
+                        var match = attribute.value.replace(/[\[]|[\]]/g, '');
+                        attributesToReplace[match] = {
+                            element: 'p-index-' + elementIndex,
+                            attribute: attribute.name
+                        };
+                        currentElement.setAttribute(attribute.name, that.data[match]);
+                    }
+                    index++;
                 }
-                getterAndSetter();
-                if (Array.isArray(array)){
-                    ['pop','push','reverse','shift','unshift','splice','sort','map','filter'].forEach( function(method){
-                        Object.defineProperty(array, method, {
-                            configurable: false,
-                            enumerable: false,
-                            writable: true,
-                            value: function () {
-                                var noReturnMethods = ['push', 'pop', 'reverse', 'shift', 'unshift'];
-                                if (noReturnMethods.indexOf(method) > -1) {
-                                    Array.prototype[method].apply(result, arguments);
-                                } else {
-                                    var results = Array.prototype[method].apply(result, arguments);
-                                    result = results;
-                                }
-                                callback.call(thisArg, result, null);
-                                getterAndSetter();
-                                return result;
-                            }
-                        });
+                elementIndex++;
+            });
+            data = main.container.innerHTML;
+            data = data.replace(/(?<!\\)(?:(\\\\)*)[\[]{2,}(\w+?)(?<!\\)(?:(\\\\)*)[\]]{2,}/g, function (match) {
+                match = match.replace(/[\[]|[\]]/g, '');
+                var index = Object.keys(that.data).indexOf(match),
+                    inAttributes = Object.keys(attributesToReplace).indexOf(match) > -1;
+                if(index > -1 && !inAttributes) {
+                    return '<p-data data-name="' + match + '">' + that.data[match] + '</p-data>';
+                } else if(!inAttributes) {
+                    return '[[' + match + ']]';
+                }
+            });
+            main.container.innerHTML = data;
+
+            arrayObserve(that.data, function(result, property){
+                if(property) {
+                    var attributeToReplace = attributesToReplace[property];
+                    if (attributeToReplace) {
+                        var element = document.querySelector('[p-attributes=' + attributeToReplace.element + ']');
+                        element.setAttribute(attributeToReplace.attribute, result[property]);
+                    }
+>>>>>>> parent of 15e24d0... Update to new version
+                }
+                replaceData(result);
+            }, null);
+        },
+        event: function(name, handler){
+            var elements = document.querySelectorAll('[p-on$=":'+name+'"]'),
+                that = this;
+            elements.forEach(function(element){
+                var attributeSplit = element.getAttribute('p-on').split(':'),
+                    eventType = attributeSplit[0],
+                    eventName = attributeSplit[1];
+
+                if(eventName = name) {
+                    element.addEventListener(eventType, function(event){
+                        var call = handler.call(element, event, that);
+                        return call;
                     });
                 }
-                return result;
-            }
+            });
         }
-
-        final.container = final.getElementFromVariable(arguments[0], document.body);
-
-        return final;
     }
 
+<<<<<<< HEAD
     return initialise.apply(this, arguments);
 }
+=======
+    main.parseTemplate();
+
+    return main;
+};
+>>>>>>> parent of 15e24d0... Update to new version
